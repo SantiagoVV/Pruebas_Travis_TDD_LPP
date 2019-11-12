d422c9a87ec9694b4d961201dba04ec74f77640d
Santiago Villar Vazquez <alu0100990522@ull.edu.es>
2019-11-12 17:26:54 +0000

    Primera prueba de los parametros

--- spec/Practica6_spec.rb
+++ spec/Practica6_spec.rb
@@ -9,11 +9,24 @@ RSpec.describe Practica6 do
 
 
   before :each do
-    @vaca = InformacionNutricional.new("vaca", 21.1, 0.0, 3.1, 50.0, 164.0)
-    @lista = List.new(nil)
+
+    @vaca = Comida.new("Vaca", 21.1, 0.0, 3.1, 50.0, 164.0, 5.6)
+  #  @cordero = Comidao.new("Cordero", 18.0, 0.0, 17.0, 20.0, 185.0, 0.0)
+  #  @camaron = Comida.new("Camaron", 17.6, 1.5, 0.6, 18.0, 2.0, 0.0)
+  #  @chocolate = Comida.new("Chocolate", 5.3, 47.0, 30.0, 2.3, 3.4, 0.0)
+  #  @salmon = Comida.new("Salmon", 19.9, 0.0, 13.6, 6.0, 3.7, 0.0)
+  #  @cerdo = Comida.new("Cerdo", 21.5, 0.0, 6.3, 7.6, 11.0, 0.0)
+  #  @pollo = Comida.new("Pollo", 20.6, 0.0, 5.6, 5.7, 7.1, 0.0)
+  #  @queso = Comida.new("Queso", 25.0, 1.3, 33.0, 11.0, 41.0, 0.0)
+  #  @cerveza = Comida.new("Cerveza", 0.5, 3.6, 0.0, 0.24, 0.22)
+  #  @leche = Comida.new("Leche", 3.3, 4.8, 3.2, 3.2, 8.9, 0.0)
+  #  @huevos = Comida.new("Huevos", 13.0, 1.1, 11.0, 4.2, 5.7, 0.0)
+  #  @cafe = Comida.new("Cafe", 0.1, 0.0, 0.0, 0.4, 0.3, 0.0)
+  #  @tofu = Comida.new("Lentejas", 23.5, 52.0, 1.4, 0.4, 3.4, 0.0)
+  #  @nuez = Comida.new("Nuez", 20.0, 21.0, 54.0, 0.3, 7.9, 0.0)
   end
 
-  it "Constructor" do
+  it "Comprobacion de parametros" do
     expect(@vaca.nombre).to eq("vaca")
     expect(@vaca.proteinas).to eq("21.1")
     expect(@vaca.carbon).to eq("0.0")
@@ -23,37 +36,6 @@ RSpec.describe Practica6 do
 
   end
 
-  it "Suma de gramos" do
-    expect(@vaca.total_gramos_hombre).to eq(54)
-    expect(@vaca.total_gramos_mujer).to eq(41)
-
-  end
-
-
-  it "Conversion de datos" do
-    expect(@vaca.calculo_glucidos).to eq(0.0)
-    expect(@vaca.calculo_proteinas).to eq(84.4)
-    expect(@vaca.calculo_lipidos).to eq(27.9)
-
-  end
-
-  it "Cantidad energitica del alimento" do
-    expect(@vaca.calculo_energetico).to eq(112.3)
-
-  end
-
-  it "Mostrar alimento formateado" do
-    expect(@cocacola.to_s).to eq("vaca, 21.1, 0.0, 3.1, 50.0, 164.0")
-  end
-
-  it "Impacto ambiental diario de un hombre" do
-
-  end
-
-  it "Impacto ambiental diario de una mujer" do
-
-
-  end
 
   it "does something useful" do
     expect(false).to eq(true)
