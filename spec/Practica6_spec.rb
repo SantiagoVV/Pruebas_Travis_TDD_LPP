1626a34b0a84c6ccfd864baf6ae49cb828c569b6
Santiago Villar Vazquez <alu0100990522@ull.edu.es>
2019-11-13 10:09:29 +0000

    Calculo de calorias mujer

--- spec/Practica6_spec.rb
+++ spec/Practica6_spec.rb
@@ -11,21 +11,20 @@ RSpec.describe "Practica6" do
 
   before :each do
 
-    @vaca = Comida.new("Vaca", 21.1, 0.0, 3.1, 50.0, 164.0, 6, 3)
+    @vaca = Comida.new("Vaca", 21.1, 0.0, 3.1, 50.0, 164.0, 6)
   #  @cordero = Comida.new("Cordero", 18.0, 0.0, 17.0, 20.0, 185.0, 0.0)
   #  @camaron = Comida.new("Camaron", 17.6, 1.5, 0.6, 18.0, 2.0, 0.0)
-    @chocolate = Comida.new("Chocolate", 5.3, 47.0, 30.0, 2.3, 3.4, 3, 3)
+    @chocolate = Comida.new("Chocolate", 5.3, 47.0, 30.0, 2.3, 3.4, 3)
   #  @salmon = Comida.new("Salmon", 19.9, 0.0, 13.6, 6.0, 3.7, 0.0)
   #  @cerdo = Comida.new("Cerdo", 21.5, 0.0, 6.3, 7.6, 11.0, 0.0)
   #  @pollo = Comida.new("Pollo", 20.6, 0.0, 5.6, 5.7, 7.1, 0.0)
   #  @queso = Comida.new("Queso", 25.0, 1.3, 33.0, 11.0, 41.0, 0.0)
-  #  @cerveza = Comida.new("Cerveza", 0.5, 3.6, 0.0, 0.24, 0.22)
+    @cerveza = Comida.new("Cerveza", 0.5, 3.6, 0.0, 0.24, 0.22, 21)
   #  @leche = Comida.new("Leche", 3.3, 4.8, 3.2, 3.2, 8.9, 0.0)
   #  @huevos = Comida.new("Huevos", 13.0, 1.1, 11.0, 4.2, 5.7, 0.0)
-  #  @cafe = Comida.new("Cafe", 0.1, 0.0, 0.0, 0.4, 0.3, 0.0)
-  #  @tofu = Comida.new("Lentejas", 23.5, 52.0, 1.4, 0.4, 3.4, 0.0)
-    @lentejas = Comida.new("Lentejas", 23.5, 52.0, 1.4, 0.4, 3.4, 3, 2)
-  #  @nuez = Comida.new("Nuez", 20.0, 21.0, 54.0, 0.3, 7.9, 0.0)
+    @cafe = Comida.new("Cafe", 0.1, 0.0, 0.0, 0.4, 0.3, 14)
+    @lentejas = Comida.new("Lentejas", 23.5, 52.0, 1.4, 0.4, 3.4, 3)
+    @nuez = Comida.new("Nuez", 20.0, 21.0, 54.0, 0.3, 7.9, 3)
   end
 
   it "Comprobacion de parametros" do
@@ -57,7 +56,7 @@ RSpec.describe "Practica6" do
   end
 
   it "Datos de comida formateados" do
-    expect(@vaca.datos_formateados).to eq(["Vaca", 506.4, 0.0, 167.4, 50.0, 164.0])
+    expect(@vaca.datos_formateados).to eq(["Vaca", 506.4, 0.0, 167.4, 50.0, 164.0, 6])
   end
 
   it "Terreno utilizado" do
@@ -69,9 +68,12 @@ RSpec.describe "Practica6" do
   end
 
   it "Cantidad energetica del hombre " do
-    expect(@vaca.calculo_energetico + @chocolate.calculo_energetico + @lentejas.calculo_energetico).to be > 3000.0
+    expect(@vaca.calculo_energetico + @chocolate.calculo_energetico + @lentejas.calculo_energetico).to be >= 3000.0
   end
 
+  it "Cantidad energetica de la  mujer " do
+    expect(@cerveza.calculo_energetico + @cafe.calculo_energetico + @nuez.calculo_energetico).to be >= 2300.0
+  end
 
 
 
